6fb7e6726fbfdacc248f95bc8ec69d97f00be057
Alexander Papashvili <sandropapashvili@gmail.com>
2022-03-02 14:01:37 +0400

    commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
                 <Styled.H1 onClick={() => navigate('/')}>Math Magicians</Styled.H1>
 
                 <Styled.Nav>
-                    <Styled.NavItem onClick={() => navigate('')}>Calculator</Styled.NavItem>
+                    <Styled.NavItem onClick={() => navigate('/')}>Calculator</Styled.NavItem>
                     <Styled.NavItem onClick={() => navigate('/Quote')}>Quote</Styled.NavItem>
                     <Styled.NavItem>
                         <Styled.A
